639725903476e391ab0b3d9a5bb91b4a32226cf8
bouzuya <m@bouzuya.net>
2016-10-18 19:21:38 +0900

    add clearInput action

--- src/intent.ts
+++ src/intent.ts
@@ -1,13 +1,20 @@
 import { ReadableStream } from 'whatwg-streams-b';
 import { filter } from 'whatwg-streams-fns/filter';
 import { map } from 'whatwg-streams-fns/map';
+import { merge } from 'whatwg-streams-fns/merge';
 import { So } from './so';
-
 
 const intent = (sources: So): ReadableStream => {
   const { DOM } = sources;
   const keydown$: ReadableStream = DOM.select('.new-todo').events('keydown');
-  const insertTodo$: ReadableStream = keydown$
+  const [keydown1$, keydown2$] = keydown$.tee();
+  const clearInput$: ReadableStream = keydown1$
+    .pipeThrough(filter((event) => {
+      const escKeyCode = 27;
+      return event.keyCode === escKeyCode;
+    }))
+    .pipeThrough(map((payload) => ({ type: 'clearInput', payload })));
+  const insertTodo$: ReadableStream = keydown2$
     .pipeThrough(filter((event) => {
       const enterKeyCode = 13;
       const trimmed = String(event.target.value).trim();
@@ -15,7 +22,7 @@ const intent = (sources: So): ReadableStream => {
     }))
     .pipeThrough(map((event) => String(event.target.value).trim()))
     .pipeThrough(map((payload) => ({ type: 'insertTodo', payload })));
-  return insertTodo$;
+  return merge(insertTodo$, clearInput$);
 };
 
 export { intent };
--- src/model.ts
+++ src/model.ts
@@ -1,26 +1,45 @@
 import { ReadableStream } from 'whatwg-streams-b';
 import { filter } from 'whatwg-streams-fns/filter';
-import { map } from 'whatwg-streams-fns/map';
 import { fold } from 'whatwg-streams-fns/fold';
+import { map } from 'whatwg-streams-fns/map';
+import { merge } from 'whatwg-streams-fns/merge';
+
+// for debug
+import { WritableStream } from 'whatwg-streams-b';
 
 const model = (action$: ReadableStream): ReadableStream => {
   const state = {
     filterFn: () => true,
     list: []
   };
-  const state$ = action$
+
+  // for debug
+  const [log$, action1$] = action$.tee();
+  log$.pipeTo(new WritableStream({
+    write(chunk) {
+      console.log(chunk);
+    }
+  }));
+
+  const [action11$, action12$] = action1$.tee();
+  const clearInput$ = action11$
+    .pipeThrough(filter((action) => action.type === 'clearInput'))
+    .pipeThrough(map((_action) => (state: any) => state));
+  const insertTodo$ = action12$
     .pipeThrough(filter((action) => action.type === 'insertTodo'))
     .pipeThrough(map((action) => {
-      return {
+      const item = {
         title: action.payload,
         completed: false
       };
-    }))
-    .pipeThrough(fold((state, item) => {
-      return Object.assign(state, { list: state.list.concat([item]) });
-    }, state));
+      return (state: any) => {
+        return Object.assign(state, { list: state.list.concat([item]) });
+      };
+    }));
+  const reducer$ = merge(clearInput$, insertTodo$);
+  const state$ = reducer$
+    .pipeThrough(fold((state, reducer) => reducer(state), state));
   return state$;
 };
 
 export { model };
-
