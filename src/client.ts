038ee84763d6a9836ddb9b9e9479df3300397e47
bouzuya <m@bouzuya.net>
2016-10-22 09:50:38 +0900

    cycle-whatwg-streams-run@0.1.0

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import { makeDOMDriver } from '@cycle/dom';
-import { run as runCycle } from './run';
+import { run as runCycle } from 'cycle-whatwg-streams-run';
 import { main } from './app';
 
 const run = (): void => {
